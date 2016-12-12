4b4994f11d671b472f38fce59eebe2c4657de786
Josh Miklos <simulatorleek@gmail.com>
2016-12-12 00:07:22 -0500

    set up and tested pipe class. works.

--- pipe-test.py
+++ pipe-test.py
@@ -1,60 +1,75 @@
-from multiprocessing import Process, Pipe
-
-def tensor_loop(conn):
-    i=0
-    while i<100000:
-        print('d')
-        if conn.poll() is True:
-            #todo: python uses pickle to send through pipe, which is slow. Use Google FlatBuffers and send_bytes.
-            i+= conn.recv()
-        conn.send(i)
-        i+=1
-    while conn.recv() != 'end':
-        pass
-    conn.close()
-
+from multiprocessing import Process, Pipe, Value
 
 class PipeClass(object):
 
-    def __init__(self, pipe_connection):
-        self.exiting = False
-        self.conn = pipe_connection
+    def __init__(self):
+        self.exiting = Value('b', 0)
 
-    def main_process(self):
-        while not self.exiting:
-            if self.conn.poll() is True:
-                in_val = self.conn.recv()
-                self.receive_func(in_val)
-            out_val = self.send_func()
+    def _other_process(self):
+        in_val = None
+        while self.exiting.value != 1:
+            if self.child_conn.poll() is True:
+                in_val = self.child_conn.recv()
+                self.child_receive_func(in_val)
+            out_val = self.child_send_func()
             if out_val is not None:
-                self.conn.send(out_val)
-        while self.conn.recv() != 'end':
-            self.exit_loop()
-        self.conn.close()
+                self.child_conn.send(out_val)
+        print('closed')
+        self.child_conn.close()
+
+    def main_process_start(self):
+        self.parent_conn, self.child_conn = Pipe(duplex=True)
+        self.p = Process(target=PipeClass._other_process, args=(self,))
+        self.p.start()
+
+    def main_process_receive(self):
+        if self.parent_conn.poll() is True:
+            in_val = self.parent_conn.recv()
+            return in_val
+        return None
+
+    def main_process_send(self, data):
+        if data is not None:
+            self.parent_conn.send(data)
 
     def soft_exit(self):
-        self.exiting = True
+        self.exiting.value = 1
+        self.p.join()
 
-    def receive_func(self, data):
+    def child_receive_func(self, data):
         pass
 
-    def send_func(self):
+    def child_send_func(self):
         pass
 
-    def exit_loop(self):
+    def child_exit_loop(self):
         pass
 
+class TensorClass(PipeClass):
+    def __init__(self):
+        super().__init__()
+        self.i=1
+        self.j=0
+
+    def inf_loop(self):
+        self.main_process_start()
+        while self.j<10:
+            self.main_process_send(self.j+1)
+            self.j_t = self.main_process_receive()
+            if self.j_t is not None:
+                self.j = self.j_t
+            print(self.j)
+        print('exited')
+        self.soft_exit()
+
+    def child_receive_func(self, data):
+        print(data)
+
+    def child_send_func(self):
+        self.i +=1
+        return self.i
 
 
 if __name__ == '__main__':
-    parent_conn, child_conn = Pipe(duplex=True)
-    p = Process(target = tensor_loop, args = (child_conn,))
-    p.start()
-    j=0
-    while j<200000:
-        if parent_conn.poll() is True:
-            print(parent_conn.recv())
-        j+=1
-        parent_conn.send(j)
-    parent_conn.send('end')
-    p.join()
+    c = TensorClass()
+    c.inf_loop()
